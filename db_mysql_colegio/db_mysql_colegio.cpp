4e4f0818c5e28659b20e038f9e8fc2b8ea3e8900
2591999 <160382603+2591999@users.noreply.github.com>
2024-05-23 04:15:16 -0600

    CRUD estudiante_BD_Colegio

---
+++ db_mysql_colegio/db_mysql_colegio.cpp
@@ -0,0 +1,29 @@
+#include "Estudiante.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string codigo, nombres, apellidos, direccion, fecha_nacimiento, id_tipo_sangre;
+	int telefono;
+
+	cout << "Ingrese el codigo: ";
+	getline(cin, codigo);
+	cout << "Ingrese Nombres: ";
+	getline(cin, nombres);
+	cout << "Ingrese Direccion: ";
+	getline(cin, direccion);
+	cout << "Ingrese Apellidos: ";
+	getline(cin, apellidos);
+	cout << "Ingrese Fecha de Nacimiento: ";
+	getline(cin, fecha_nacimiento);
+	cout << "Ingrese el Id del tipo de sangre: ";
+	getline(cin, id_tipo_sangre);
+
+	Estudiante E = Estudiante(nombres, apellidos, direccion, fecha_nacimiento, id_tipo_sangre);
+	c.crear();
+	c.leer();
+
+	system("pause");
+	return 0;
+}
